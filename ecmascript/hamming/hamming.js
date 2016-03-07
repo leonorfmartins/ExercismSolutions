4cdd8ec65fb382429e0682005e8c2b63946b6078
Leonor Oliveira <leonorfmartins@gmail.com>
2016-03-08 00:54:46 +0100

    improved solution using reduce

--- ecmascript/hamming/hamming.js
+++ ecmascript/hamming/hamming.js
@@ -1,12 +1,12 @@
 export default class Hamming {
   compute(source, toCompare) {
-    if (source.length !== toCompare.length) {throw new Error("DNA strands must be of equal length.");}
-    let distance = 0;
-    for (let i = 0; i <= source.length; i++) {
-      if (source[i] != toCompare[i]) {
-        distance++;
+    if (source.length !== toCompare.length) throw new Error("DNA strands must be of equal length.");
+    let distance = source.split('').reduce((distance, char, i) => {
+      if (char != toCompare[i]) {
+        return distance+1;
       }
-    }
+      return distance;
+    }, 0);
     return distance;
   }
 }
