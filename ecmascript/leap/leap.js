25c25a98b0c123444c9126ffd8c573a4dfde21dd
Leonor Oliveira <leonorfmartins@gmail.com>
2016-03-08 00:08:57 +0100

    changed syntax for better ES6 understanding.

--- ecmascript/leap/leap.js
+++ ecmascript/leap/leap.js
@@ -2,15 +2,8 @@ export default function(year) {
   return isDivisibleBy4(year) && (isDivisibleBy100(year) || isDivisibleBy400(year));
 }
 
-const isDivisibleBy4 = (year) => {
-  return year % 4 == 0;
-};
+const isDivisibleBy4 = (year) => year % 4 == 0;
 
-const isDivisibleBy100 = (year) => {
-  return year % 100 != 0;
-};
+const isDivisibleBy100 = (year) => year % 100 != 0;
 
-const isDivisibleBy400 = (
-  year) => {
-  return year % 400 === 0;
-};
+const isDivisibleBy400 = (year) => year % 400 === 0;
