dfc24d8ce77650c95bd21a62375b3815e2acd089
Leonor Martins <leonorfmartins@gmail.com>
2016-07-11 22:39:39 +0200

    Better solution without using Map

--- ecmascript/word-count/word-count.js
+++ ecmascript/word-count/word-count.js
@@ -2,27 +2,20 @@ export default class words {
   count(input) {
     let splittedWords = this.splitWords(input);
     let mappedWords = splittedWords.reduce((mappedWords, word) => {
-        let wordCounter = mappedWords.get(word);
-        if (wordCounter) {
-          mappedWords.set(word, wordCounter + 1)
-      } else {
-          mappedWords.set(word, 1);
-      }
+        let wordCounter = mappedWords[word]
+        if (mappedWords.hasOwnProperty(word)) {
+          mappedWords[word] =  wordCounter + 1;
+        } else {
+          mappedWords[word] = 1;
+        }
       return mappedWords;
-    }, new Map());
-    return this.mapToObject(mappedWords);
+    }, {});
+    return mappedWords;
   }
 
   splitWords(input) {
      return input.replace(/\s+/g, ' ').trim().split(' ');
   }
 
-  mapToObject(mappedWords) {
-    let countedWordObject = {};
-    mappedWords.forEach((value, key) => {
-        countedWordObject[key] = value
-    });
-    return countedWordObject;
-  }
 }
 
